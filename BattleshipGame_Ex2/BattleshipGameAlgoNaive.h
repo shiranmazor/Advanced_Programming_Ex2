e8bb376dfba2ab3c461849705aa9c2ddec52754f
shiranmazor <shiran.mazpr@gmail.com>
2017-04-29 12:43:03 +0300

    df

--- BattleshipGame_Ex2/BattleshipGameAlgoNaive.h
+++ BattleshipGame_Ex2/BattleshipGameAlgoNaive.h
@@ -18,4 +18,9 @@ public:
 	// Blocking Copy and Assignment
 	BattleshipGameAlgoNaive(const BattleshipGameAlgoNaive&) = delete;
 	BattleshipGameAlgoNaive& operator = (const BattleshipGameAlgoNaive&) = delete;
+
+	virtual void setBoard(int player, const char** board, int numRows, int numCols) override;
+	virtual bool init(const std::string& path) override;
+	virtual std::pair<int, int> attack() override;
+	virtual void notifyOnAttackResult(int player, int row, int col, AttackResult result) override;
 };
--- BattleshipGame_Ex2/BattleshipGameAlgoSmart.h
+++ BattleshipGame_Ex2/BattleshipGameAlgoSmart.h
@@ -11,5 +11,17 @@ using namespace std;
 
 class BattleshipGameAlgoSmart : public IBattleshipGameAlgo
 {
+public:
+	Player playerName;
+	BattleBoard* playerBoard = nullptr;
+
+	// Blocking Copy and Assignment
+	BattleshipGameAlgoSmart(const BattleshipGameAlgoSmart&) = delete;
+	BattleshipGameAlgoSmart& operator = (const BattleshipGameAlgoSmart&) = delete;
+
+	virtual void setBoard(int player, const char** board, int numRows, int numCols) override;
+	virtual bool init(const std::string& path) override;
+	virtual std::pair<int, int> attack() override;
+	virtual void notifyOnAttackResult(int player, int row, int col, AttackResult result) override;
 
 };
